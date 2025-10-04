e12ef331a56ff4de16ca3118c4685fceaa782fe5
shalina maniyar <shalinamaniyar@gmail.com>
2025-10-04 08:39:14 +0530

    version11

--- stgrade.py
+++ stgrade.py
@@ -9,3 +9,10 @@ elif marks>=60:
     print("grade D")
 else:
     print("fail")
+    m=int(input("enter marks of 1 subject"))
+    n=int(input("enter marks of 2 subject"))
+    o=int(input("enter marks of 3 subject"))    
+    p=int(input("enter marks of 4 subject"))
+    q=int(input("enter marks of 5 subject"))
+    total=m+n+o+p+q/5
+    print("average:",total)
